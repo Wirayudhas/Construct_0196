5d3c7d8434d9a066127aacd9bdee68c65c224740
Wirayudhas <arturwirayuda@gmail.com>
2024-05-14 14:36:16 +0700

    Chain Function

---
+++ ChainFunction/ChainFunction.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class angka {
+private:
+	int* arr;
+	int panjang;
+public:
+	angka(int); //constructor
+	~angka();
+	void cetakData();
+	void isiData();
+};
+
+//Definisi Member Function
+angka::angka(int i) { //Constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+
+angka::~angka() {//Destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array sudah dilepaskan" << endl;
+}
+
+void angka::isiData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = "; cin >> arr[i];
+	}
+	cout << endl;
+}
+
+int main() {
+	angka belajarcpp(3); //Constructor Dipanggil
+	angka* ptrBelajarcpp = new angka(5); //Constractor Dipanggil
+	delete ptrBelajarcpp; //Destructor Dipanggil
+
+	return 0;
+}//Destructor Dipanggil
